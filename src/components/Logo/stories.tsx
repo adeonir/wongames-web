ce72412da58ecf378d660d7ce67f0b3c0e824f71
Adeonir Kohl <adeonir@gmail.com>
2020-09-06 00:11:08 -0300

    Add args and props to logo story

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,10 +1,11 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
 
 import { Logo } from '.'
+import { LogoProps } from '.'
 
 export default {
   title: 'Logo',
   component: Logo,
 } as Meta
 
-export const Basic: Story = () => <Logo />
+export const Basic: Story<LogoProps> = (args) => <Logo {...args} />
