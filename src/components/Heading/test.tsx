7673dedf9530030fad33a83f00e97f1dc1d7e98d
Adeonir Kohl <adeonir@gmail.com>
2020-09-14 22:50:04 -0300

    Revert styled-components to 5.1.0 and update tests

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -6,26 +6,23 @@ import { Heading } from '.'
 describe('<Heading />', () => {
   it('should render a white heading by default', () => {
     renderWithTheme(<Heading>Won Games</Heading>)
-    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
-      'color',
-      '#fafafa',
-    )
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      color: '#fafafa',
+    })
   })
 
   it('should render a black heading when color is passed', () => {
     renderWithTheme(<Heading color="black">Won Games</Heading>)
-    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
-      'color',
-      '#030517',
-    )
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      color: '#030517',
+    })
   })
 
   it('should render a heading with a line on the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
-    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
-      'border-left',
-      '0.7rem solid #3cd3c1',
-    )
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3cd3c1',
+    })
   })
 
   it('should render a heading with a line at the bottom', () => {
