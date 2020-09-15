216cd2de98048ba9e9d450fd450fcc3742516448
Adeonir Kohl <adeonir@gmail.com>
2020-09-14 23:58:06 -0300

    Add button tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -4,11 +4,27 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import { Button } from '.'
 
 describe('<Button />', () => {
-  it('should render the button', () => {
-    const {
-      container: { firstChild },
-    } = renderWithTheme(<Button>Buy now</Button>)
-    expect(screen.getByRole('button', { name: /buy now/i })).toBeInTheDocument()
-    expect(firstChild).toMatchSnapshot()
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '4rem',
+    })
+  })
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '3rem',
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '5rem',
+    })
   })
 })
