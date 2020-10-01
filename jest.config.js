bcfbd2a3c84ed74c0a0b19650714fccd1869888c
Adeonir Kohl <adeonir@gmail.com>
2020-10-01 17:55:31 -0300

    Bump dependencies and update styled-components jest config

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,8 @@ module.exports = {
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/config.ts'],
   modulePaths: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
+  },
 }
