d0e4a155d8d22282eed852429a740a305e6fa7cd
Adeonir Kohl <adeonir@gmail.com>
2020-10-20 14:01:48 -0300

    Optimizating tests

--- src/containers/Home/test.tsx
+++ src/containers/Home/test.tsx
@@ -1,35 +1,57 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { screen } from '@testing-library/react'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightsMock from 'components/Highlight/mock'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import { Home } from '.'
 
-describe('<Home />', () => {
-  it.skip('should render menu and footer', () => {
-    const { container } = renderWithTheme(<Home />)
+const props = {
+  banners: bannersMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightsMock,
+  mostPopularGames: [gamesMock[0]],
+  upCommingGames: [gamesMock[0]],
+  upCommingHighlight: highlightsMock,
+  upCommingMoreGames: [gamesMock[0]],
+  freeGamesHighlight: highlightsMock,
+  freeGames: [gamesMock[0]],
+}
 
+describe('<Home />', () => {
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home {...props} />)
+
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
 
+    // footer
     expect(
-      screen.getByRole('heading', { name: /contact/i }),
+      screen.getByRole('heading', { name: /follow us/i }),
     ).toBeInTheDocument()
-  })
 
-  it.skip('should render the sections', () => {
-    const { container } = renderWithTheme(<Home />)
+    // logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
 
+    // sections
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
-
     expect(
       screen.getByRole('heading', { name: /most popular/i }),
     ).toBeInTheDocument()
-
     expect(
       screen.getByRole('heading', { name: /upcomming/i }),
     ).toBeInTheDocument()
-
     expect(
       screen.getByRole('heading', { name: /free games/i }),
     ).toBeInTheDocument()
+
+    // banners
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+
+    // game cards
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+
+    // highlights
+    expect(screen.getAllByText(/red dead is back/i)).toHaveLength(3)
   })
 })
