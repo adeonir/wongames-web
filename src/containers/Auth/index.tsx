19261f8d8cb5a921e687562b7a7ef6ce81270488
Adeonir Kohl <adeonir@gmail.com>
2021-03-10 00:25:08 -0300

    Finish banner section of auth container

--- src/containers/Auth/index.tsx
+++ src/containers/Auth/index.tsx
@@ -10,22 +10,30 @@ type AuthProps = {
 export const Auth = ({ title, children }: AuthProps) => (
   <S.AuthContainer>
     <S.Banner>
-      <Logo />
+      <S.BannerWrapper>
+        <Logo id="banner" />
 
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
-      <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+        <div>
+          <Heading size="huge">
+            All your favorite <br /> games in one place
+          </Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
+        <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerWrapper>
     </S.Banner>
 
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
+      <S.ContentWrapper>
+        <Logo id="content" color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
 
-      {children}
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.AuthContainer>
 )
--- src/containers/Auth/styles.ts
+++ src/containers/Auth/styles.ts
@@ -1,11 +1,104 @@
-import styled from 'styled-components'
+import * as HeadingStyles from 'components/Heading/styles'
+import * as LogoStyles from 'components/Logo/styles'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const AuthContainer = styled.div``
+export const AuthContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1.1fr 1fr;
+  height: 100vh;
 
-export const Banner = styled.div``
+  ${media.lessThan('medium')`
+    grid-template-columns: 1fr;
+  `}
+`
 
-export const Subtitle = styled.h3``
+export const Banner = styled.div`
+  ${({ theme }) => css`
+    background-image: url(/assets/auth-banner.png);
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    color: ${theme.colors.white};
+    padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
+      ${theme.spacings.large};
 
-export const Footer = styled.p``
+    ${media.lessThan('medium')`
+      display: none;
+    `}
 
-export const Content = styled.div``
+    &::after {
+      position: absolute;
+      content: '';
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background: ${theme.colors.black};
+      opacity: 0.85;
+    }
+  `}
+`
+
+export const BannerWrapper = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    z-index: ${theme.layers.base};
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-content: space-between;
+    height: 100%;
+
+    ${HeadingStyles.HeadingContainer} {
+      line-height: calc(${theme.font.sizes.huge} + 1rem);
+    }
+  `}
+`
+
+export const Subtitle = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+    font-weight: ${theme.font.light};
+    margin-top: ${theme.spacings.small};
+
+    strong {
+      color: ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Footer = styled.p`
+  ${({ theme }) => css`
+    align-self: end;
+    text-align: center;
+    font-size: ${theme.font.sizes.small};
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `}
+`
+
+export const ContentWrapper = styled.div`
+  ${({ theme }) => css`
+    width: 30rem;
+
+    ${media.greaterThan('medium')`
+      width: 36rem;
+    `}
+
+    ${LogoStyles.LogoContainer} {
+      margin: 0 auto ${theme.spacings.xxlarge};
+    }
+
+    ${HeadingStyles.HeadingContainer} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+  `}
+`
