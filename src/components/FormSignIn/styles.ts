9fccc1c6407ee666a53f2c3811a45f0d56ddc579
Adeonir Kohl <adeonir@gmail.com>
2021-03-12 21:33:37 -0300

    Add styles to form sign in

--- src/components/FormSignIn/styles.ts
+++ src/components/FormSignIn/styles.ts
@@ -1,7 +1,46 @@
-import styled from 'styled-components'
+import * as ButtonStyles from 'components/Button/styles'
+import * as TextFieldStyles from 'components/TextField/styles'
+import { darken } from 'polished'
+import styled, { css } from 'styled-components'
 
-export const FormSignInContainer = styled.div``
+export const FormSignInContainer = styled.div`
+  ${({ theme }) => css`
+    ${TextFieldStyles.TextFieldContainer} {
+      margin: ${theme.spacings.xxsmall} 0;
+    }
 
-export const ForgotPassword = styled.a``
+    ${ButtonStyles.ButtonContainer} {
+      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+    }
+  `}
+`
 
-export const FormLink = styled.div``
+export const ForgotPassword = styled.a`
+  ${({ theme }) => css`
+    display: block;
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-decoration: none;
+    text-align: right;
+  `}
+`
+
+export const FormLink = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-align: center;
+
+    a {
+      color: ${theme.colors.secondary};
+      text-decoration: none;
+      border-bottom: 0.1rem solid ${theme.colors.secondary};
+      transition: color, border, ${theme.transition.fast};
+
+      &:hover {
+        color: ${darken(0.1, theme.colors.secondary)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+      }
+    }
+  `}
+`
