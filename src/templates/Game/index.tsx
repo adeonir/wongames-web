2029ce93115a9c17987f627426014e1cd8589228
Adeonir Kohl <adeonir@gmail.com>
2021-03-21 12:22:08 -0300

    Add cover image

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -8,6 +8,11 @@ export type GameProps = {
 
 export const Game = ({ children }: GameProps) => (
   <Base>
+    <S.Cover
+      src="https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg"
+      role="image"
+      aria-label="cover"
+    />
     <S.GameContainer>{children}</S.GameContainer>
   </Base>
 )
--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -1,3 +1,28 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const GameContainer = styled.div``
+
+type CoverProps = {
+  src: string
+}
+
+export const Cover = styled.div<CoverProps>`
+  ${({ src }) => css`
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 40rem;
+    background-image: url(${src});
+    background-size: cover;
+    background-position: top center;
+    opacity: 0.4;
+
+    ${media.greaterThan('medium')`
+      height: 70rem;
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%);
+    `}
+  `}
+`
