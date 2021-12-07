be3ee421f9900b5bdad05537aa458144c5b9ca35
Adeonir Kohl <adeonir@gmail.com>
2021-12-07 00:29:52 -0300

    feat: get games from api

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,46 @@
+import { gql } from '@apollo/client'
+import { initializeApollo } from 'services'
+
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
 
 export default function Games(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
 export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGames {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
+        }
+      }
+    `,
+  })
+
   return {
     props: {
-      games: gamesMock,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(game.price),
+      })),
       filterItems: filterItemsMock,
     },
   }
