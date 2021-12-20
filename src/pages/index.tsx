f5d3222eb803a41c6cedecb171e8a48fc3a35b5e
Adeonir Kohl <adeonir@gmail.com>
2021-12-20 13:44:44 -0300

    feat: add dynamic highlights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,15 @@ export const getStaticProps: GetStaticProps = async () => {
         price: game.price,
       })),
       popularGamesTitle: sections?.popularGames?.title,
-      popularGamesHighlight: highlightMock,
+      popularGamesHighlight: {
+        title: sections?.popularGames?.highlight?.title,
+        subtitle: sections?.popularGames?.highlight?.subtitle,
+        backgroundImage: `http://localhost:1337${sections?.popularGames?.highlight?.background?.url}`,
+        floatImage: `http://localhost:1337${sections?.popularGames?.highlight?.floatImage?.url}`,
+        buttonLabel: sections?.popularGames?.highlight?.buttonLabel,
+        buttonLink: `/games/${sections?.popularGames?.highlight?.buttonLink}`,
+        alignment: sections?.popularGames?.highlight?.alignment,
+      },
       popularGames: sections?.popularGames!.games!.map((game) => ({
         title: game.name,
         slug: game.slug,
@@ -58,7 +66,15 @@ export const getStaticProps: GetStaticProps = async () => {
         img: `http://localhost:1337${game.cover?.url}`,
         price: game.price,
       })),
-      upcomingHighlight: highlightMock,
+      upcomingHighlight: {
+        title: sections?.upcomingGames?.highlight?.title,
+        subtitle: sections?.upcomingGames?.highlight?.subtitle,
+        backgroundImage: `http://localhost:1337${sections?.upcomingGames?.highlight?.background?.url}`,
+        floatImage: `http://localhost:1337${sections?.upcomingGames?.highlight?.floatImage?.url}`,
+        buttonLabel: sections?.upcomingGames?.highlight?.buttonLabel,
+        buttonLink: `/games/${sections?.upcomingGames?.highlight?.buttonLink}`,
+        alignment: sections?.upcomingGames?.highlight?.alignment,
+      },
       freeGamesTitle: sections?.freeGames?.title,
       freeGames: freeGames.map((game) => ({
         title: game.name,
@@ -67,7 +83,15 @@ export const getStaticProps: GetStaticProps = async () => {
         img: `http://localhost:1337${game.cover?.url}`,
         price: game.price,
       })),
-      freeHighlight: highlightMock,
+      freeHighlight: {
+        title: sections?.freeGames?.highlight?.title,
+        subtitle: sections?.freeGames?.highlight?.subtitle,
+        backgroundImage: `http://localhost:1337${sections?.freeGames?.highlight?.background?.url}`,
+        floatImage: `http://localhost:1337${sections?.freeGames?.highlight?.floatImage?.url}`,
+        buttonLabel: sections?.freeGames?.highlight?.buttonLabel,
+        buttonLink: `/games/${sections?.freeGames?.highlight?.buttonLink}`,
+        alignment: sections?.freeGames?.highlight?.alignment,
+      },
     },
   }
 }
