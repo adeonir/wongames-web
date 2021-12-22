85ac90549e91814b258b6f232758114380e82e47
Adeonir Kohl <adeonir@gmail.com>
2021-12-21 23:35:32 -0300

    feat: add function to parse query string

---
+++ src/utils/filter/test.ts
@@ -0,0 +1,40 @@
+import { parseQueryStringToSearch, parseQueryStringToFilter } from '.'
+
+const filterItems = [
+  { name: 'price_lte', type: 'radio' },
+  { name: 'platforms', type: 'checkbox' },
+  { name: 'developers', type: 'checkbox' },
+  { name: 'sort', type: 'radio' },
+]
+
+const queryString = {
+  price_lte: 100,
+  platforms: ['windows', 'linux'],
+  developers: 'Rockstar Games',
+  sort: 'price:asc',
+}
+
+describe('parseQueryStringToSearch', () => {
+  it('should parse the queryString to JSON format', () => {
+    const parsedQuery = parseQueryStringToSearch({ queryString, filterItems })
+
+    expect(parsedQuery).toStrictEqual({
+      price_lte: 100,
+      platforms: { name_contains: ['windows', 'linux'] },
+      developers: { name_contains: 'Rockstar Games' },
+    })
+  })
+})
+
+describe('parseQueryStringToFilter', () => {
+  it('should parse the queryString to query string', () => {
+    const parsedQuery = parseQueryStringToFilter({ queryString, filterItems })
+
+    expect(parsedQuery).toStrictEqual({
+      price_lte: 100,
+      platforms: ['windows', 'linux'],
+      developers: ['Rockstar Games'],
+      sort: 'price:asc',
+    })
+  })
+})
