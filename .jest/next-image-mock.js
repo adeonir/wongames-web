4affaaa06396debe041c60014b57dbca5c62d823
Adeonir Kohl <adeonir@gmail.com>
2022-01-10 23:30:15 -0300

    fix: revert next-image mock

--- .jest/next-image-mock.js
+++ .jest/next-image-mock.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import * as NextImage from 'next/image'
 
-const OriginalNextImage = NextImage.default
-
 Object.defineProperty(NextImage, 'default', {
   configurable: true,
-  value: (props) => <OriginalNextImage {...props} unoptimized />,
+  value: (props) => {
+    const { objectFit, ...rest } = props
+    return <img {...rest} />
+  },
 })
