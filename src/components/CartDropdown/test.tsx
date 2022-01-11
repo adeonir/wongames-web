b506536b8459452fd97117511a9077721c25db86
Adeonir Kohl <adeonir@gmail.com>
2022-01-10 23:44:18 -0300

    feat: add context to CartDropdown and remove prop drilling

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -1,21 +1,29 @@
 import { render, screen } from 'utils/tests'
 
 import items from 'components/CartList/mock'
+import { CartContextDefaultValues } from 'hooks'
 
 import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
-  it('should render <CartIcon /> and its badge', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />)
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: 'R$ 330,00',
+    }
 
+    render(<CartDropdown />, { cartProviderProps })
+  })
+
+  it('should render <CartIcon /> and its badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render Dropdown content with cart items and total', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />)
-
-    expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
+    expect(screen.getByText('R$ 330,00')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
 })
