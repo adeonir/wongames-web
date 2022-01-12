5f5620f28531add3e42fc4094c41c3002e0a2ee5
Adeonir Kohl <adeonir@gmail.com>
2022-01-12 00:19:14 -0300

    feat: add remove button to GameItem and make some fixes

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Download } from '@styled-icons/boxicons-solid/Download'
 
+import { useCart } from 'hooks'
+
 import { formatPrice } from 'utils/formatters'
 
 import * as S from './styles'
@@ -13,6 +15,7 @@ export type PaymentInfoProps = {
 }
 
 export type GameItemProps = {
+  id: string
   img: string
   title: string
   price: number
@@ -21,50 +24,60 @@ export type GameItemProps = {
 }
 
 const GameItem = ({
+  id,
   img,
   title,
   price,
   downloadLink,
   paymentInfo,
-}: GameItemProps) => (
-  <S.Wrapper>
-    <S.GameContent>
-      <S.ImageBox>
-        <Image src={img} alt={title} width={150} height={70} />
-      </S.ImageBox>
+}: GameItemProps) => {
+  const { isInCart, removeFromCart } = useCart()
 
-      <S.Content>
-        <S.Title>
-          {title}
-          {!!downloadLink && (
-            <S.DownloadLink
-              href={downloadLink}
-              target="_blank"
-              aria-label={`Get ${title} here`}
-            >
-              <Download size={22} />
-            </S.DownloadLink>
-          )}
-        </S.Title>
-        <S.Price>{formatPrice(price)}</S.Price>
-      </S.Content>
-    </S.GameContent>
+  return (
+    <S.Wrapper>
+      <S.GameContent>
+        <S.ImageBox>
+          <Image src={img} alt={title} width={150} height={70} />
+        </S.ImageBox>
 
-    {!!paymentInfo && (
-      <S.PaymentContent>
-        <p>{paymentInfo.purchaseDate}</p>
-        <S.CardInfo>
-          <span>{paymentInfo.number}</span>
-          <Image
-            src={paymentInfo.img}
-            alt={paymentInfo.flag}
-            width={38}
-            height={24}
-          />
-        </S.CardInfo>
-      </S.PaymentContent>
-    )}
-  </S.Wrapper>
-)
+        <S.Content>
+          <S.Title>
+            {title}
+            {!!downloadLink && (
+              <S.DownloadLink
+                href={downloadLink}
+                target="_blank"
+                aria-label={`Get ${title} here`}
+              >
+                <Download size={22} />
+              </S.DownloadLink>
+            )}
+          </S.Title>
+          <S.Group>
+            <S.Price>{formatPrice(price)}</S.Price>
+            {isInCart(id) && (
+              <S.Remove onClick={() => removeFromCart(id)}>Remove</S.Remove>
+            )}
+          </S.Group>
+        </S.Content>
+      </S.GameContent>
+
+      {!!paymentInfo && (
+        <S.PaymentContent>
+          <p>{paymentInfo.purchaseDate}</p>
+          <S.CardInfo>
+            <span>{paymentInfo.number}</span>
+            <Image
+              src={paymentInfo.img}
+              alt={paymentInfo.flag}
+              width={38}
+              height={24}
+            />
+          </S.CardInfo>
+        </S.PaymentContent>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default GameItem
