6282637089302d2606c7f56f4d5ddeabd0e65e46
Adeonir Kohl <adeonir@gmail.com>
2022-01-13 00:12:16 -0300

    feat: add register mutation

---
+++ src/graphql/types/MutationRegister.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { UsersPermissionsRegisterInput } from './globalTypes'
+
+// ====================================================
+// GraphQL mutation operation: MutationRegister
+// ====================================================
+
+export interface MutationRegister_register {
+  jwt: string | null
+}
+
+export interface MutationRegister {
+  register: MutationRegister_register
+}
+
+export interface MutationRegisterVariables {
+  input: UsersPermissionsRegisterInput
+}
