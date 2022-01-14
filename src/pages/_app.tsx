b143b3694eccc969e4ced8b13993996b6662bc91
Adeonir Kohl <adeonir@gmail.com>
2022-01-13 23:32:31 -0300

    feat: add auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import ProgressBar from 'nextjs-progressbar'
-import Head from 'next/head'
-import { AppProps } from 'next/app'
+
+import { Provider as AuthProvider } from 'next-auth/client'
 import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 import { CartProvider } from 'hooks/use-cart'
+
+import Head from 'next/head'
+import { AppProps } from 'next/app'
 
 import { useApollo } from 'utils/apollo'
 
@@ -13,29 +16,31 @@ export default function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="The best Game Stores in the world!"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="The best Game Stores in the world!"
+              />
+            </Head>
+            <GlobalStyles />
+            <ProgressBar
+              color="#f231a5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
             />
-          </Head>
-          <GlobalStyles />
-          <ProgressBar
-            color="#f231a5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
