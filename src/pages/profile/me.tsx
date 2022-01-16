389ae1610dd00501f27f8ecfd24d37a913ab2fc7
Adeonir Kohl <adeonir@gmail.com>
2022-01-16 00:05:39 -0300

    feat: add protected routes

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,6 +1,10 @@
+import { GetServerSidePropsContext } from 'next'
+
 import ProfileTemplate from 'templates/Profile'
 
 import FormProfile from 'components/FormProfile'
+
+import { protectedRoutes } from 'utils/protected-routes'
 
 export default function Me() {
   return (
@@ -8,4 +12,12 @@ export default function Me() {
       <FormProfile />
     </ProfileTemplate>
   )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+
+  return {
+    props: { session },
+  }
 }
