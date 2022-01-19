7d7bd4402e160624cd4719fc9661f18b6c93c811
Adeonir Kohl <adeonir@gmail.com>
2022-01-18 22:30:11 -0300

    feat: sent forgot password email

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -25,7 +25,9 @@ export const FormLoading = styled.img.attrs(() => ({
 
 export const FormError = styled.div`
   ${({ theme }) => css`
-    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     color: red;
     font-size: ${theme.font.sizes.small};
     svg {
@@ -50,6 +52,18 @@ export const FormLink = styled.div`
         color: ${darken(0.1, theme.colors.secondary)};
         border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
       }
+    }
+  `}
+`
+
+export const FormSuccess = styled.p`
+  ${({ theme }) => css`
+    text-align: center;
+    color: ${theme.colors.secondary};
+    font-size: ${theme.font.sizes.small};
+    svg {
+      color: ${theme.colors.secondary};
+      width: 2.4rem;
     }
   `}
 `
