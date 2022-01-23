a3f667096c5452aba38060586e637bf88ed98f79
Adeonir Kohl <adeonir@gmail.com>
2022-01-23 18:42:22 -0300

    feat: add fetch to useeffect

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -8,20 +8,49 @@ import Heading from 'components/Heading'
 import { useCart } from 'hooks'
 
 import * as S from './styles'
+import { createPaymentIntent } from 'utils/stripe'
+import { Session } from 'next-auth'
 
-const PaymentForm = () => {
+type PaymentFormProps = {
+  session: Session
+}
+
+const PaymentForm = ({ session }: PaymentFormProps) => {
   const { items } = useCart()
 
   const [error, setError] = useState<string | null>(null)
   const [disabled, setDisabled] = useState(true)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [clientSecret, setClientSecret] = useState('')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [freeGames, setFreeGames] = useState(false)
 
   useEffect(() => {
-    if (items.length) {
-      // ..
+    async function setPaymentMode() {
+      if (items.length) {
+        const data = await createPaymentIntent({
+          items,
+          token: session.jwt as string,
+        })
+
+        if (data.freeGames) {
+          setFreeGames(true)
+          console.info(data.freeGames)
+          return
+        }
+
+        if (data.error) {
+          setError(data.error)
+          return
+        }
+
+        setClientSecret(data.clientSecret)
+        console.info(data.clientSecret)
+      }
     }
-  }, [items])
+
+    setPaymentMode()
+  }, [items, session])
 
   const handleChange = async (event: StripeCardElementChangeEvent) => {
     setDisabled(event.empty)
