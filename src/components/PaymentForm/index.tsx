1866979ce670d53a38a38304a244f6ed9ec80f90
Adeonir Kohl <adeonir@gmail.com>
2022-01-23 17:48:03 -0300

    feat: add custom style to card element

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -22,7 +22,12 @@ const PaymentForm = () => {
         </Heading>
 
         <CardElement
-          options={{ hidePostalCode: true }}
+          options={{
+            hidePostalCode: true,
+            style: {
+              base: { fontSize: '16px' },
+            },
+          }}
           onChange={handleChange}
         />
 
