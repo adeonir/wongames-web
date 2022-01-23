204b2b7822aa99d3314d28aeb42f915595fe0fbc
Adeonir Kohl <adeonir@gmail.com>
2022-01-23 17:17:11 -0300

    refactor: delete unused card stuff

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -9,9 +9,6 @@ import { protectedRoutes } from 'utils/protected-routes'
 import { QueryRecommended } from 'graphql/types'
 import { QUERY_RECOMMENDED } from 'graphql/queries'
 
-import cardsMock from 'components/PaymentOptions/mock'
-import itemsMock from 'components/CartList/mock'
-
 export default function Cart(props: CartTemplateProps) {
   return <CartTemplate {...props} />
 }
@@ -27,9 +24,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       session,
-      items: itemsMock,
-      total: '$ 430,00',
-      cards: cardsMock,
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
