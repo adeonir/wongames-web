5277cc8d3e29f7114aa1beb601179387f4de6573
Adeonir Kohl <adeonir@gmail.com>
2022-01-25 21:17:58 -0300

    refactor: add fetcher method

--- src/utils/stripe/index.tsx
+++ src/utils/stripe/index.tsx
@@ -1,6 +1,27 @@
+import { PaymentIntent } from '@stripe/stripe-js'
+
 import { CartItem } from 'hooks'
 
-type PaymentIntentProps = {
+type FetcherParams = {
+  url: string
+  body: string
+  token: string
+}
+
+const fetcher = async ({ url, body, token }: FetcherParams) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body,
+  })
+
+  return await response.json()
+}
+
+type PaymentIntentParams = {
   items: CartItem[]
   token: string
 }
@@ -8,18 +29,32 @@ type PaymentIntentProps = {
 export const createPaymentIntent = async ({
   items,
   token,
-}: PaymentIntentProps) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/orders/create-payment-intent`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ cart: items }),
-    }
-  )
+}: PaymentIntentParams) => {
+  return fetcher({
+    url: 'orders/create-payment-intent',
+    body: JSON.stringify({ cart: items }),
+    token,
+  })
+}
 
-  return await response.json()
+type CreatePaymentParams = {
+  items: CartItem[]
+  paymentIntent?: PaymentIntent
+  token: string
+}
+
+export const createPayment = async ({
+  items,
+  paymentIntent,
+  token,
+}: CreatePaymentParams) => {
+  return fetcher({
+    url: 'orders',
+    body: JSON.stringify({
+      cart: items,
+      paymentIntentId: paymentIntent?.id,
+      paymentMethod: paymentIntent?.payment_method,
+    }),
+    token,
+  })
 }
