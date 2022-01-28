d5f28dea22cac093493e0443e961ae52decff06d
Adeonir Kohl <adeonir@gmail.com>
2022-01-28 12:14:02 -0300

    feat: config cypress to use typrescript

---
+++ cypress/integration/example.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="../support/index.d.ts" />
+
+describe('Cypress TS', () => {
+  it('should visit google page', () => {
+    cy.google()
+    cy.url().should('include', 'google')
+  })
+})
