e6d79b192d1cd825a6fc2c21b35318dbd3b98d9d
Adeonir Kohl <adeonir@gmail.com>
2022-01-30 19:43:35 -0300

    test: find the first heading

--- cypress/integration/home.ts
+++ cypress/integration/home.ts
@@ -3,5 +3,10 @@
 describe('Home page', () => {
   it('should render home page sections', () => {
     cy.visit('/')
+
+    cy.get('.slick-slider').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i })
+      cy.findByRole('link', { name: /buy now/i })
+    })
   })
 })
