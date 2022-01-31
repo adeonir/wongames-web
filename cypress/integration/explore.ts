5dd2ad97ce39a930512502daec8488eb4a885fb0
Adeonir Kohl <adeonir@gmail.com>
2022-01-31 17:34:19 -0300

    test: sort games by price

--- cypress/integration/explore.ts
+++ cypress/integration/explore.ts
@@ -29,4 +29,24 @@ describe('Explore page', () => {
     cy.findByRole('button', { name: /Show more/i }).click()
     cy.getByDataCy('game-card').should('have.length', 30)
   })
+
+  it('should order by price', () => {
+    cy.findByText(/Lowest to highest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText('FREE').should('exist')
+      })
+
+    cy.findByText(/Highest to lowest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText('FREE').should('not.exist')
+      })
+  })
 })
