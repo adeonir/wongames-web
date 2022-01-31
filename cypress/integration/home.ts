e19d91e39d3f90c68257dbc28778a62ea2f7594b
Adeonir Kohl <adeonir@gmail.com>
2022-01-31 09:59:26 -0300

    test: using assertions

--- cypress/integration/home.ts
+++ cypress/integration/home.ts
@@ -3,6 +3,12 @@
 describe('Home page', () => {
   it('should render home page sections', () => {
     cy.visit('/')
+
     cy.renderBanner()
+
+    cy.renderShowCase({ name: 'New Games' })
+    cy.renderShowCase({ name: 'Popular Games' })
+    cy.renderShowCase({ name: 'Upcoming' })
+    cy.renderShowCase({ name: 'Free Games' })
   })
 })
