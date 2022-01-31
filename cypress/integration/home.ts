5700496193097cb2df6b80ccdc801331c238d903
Adeonir Kohl <adeonir@gmail.com>
2022-01-30 23:57:10 -0300

    test: create custom command

--- cypress/integration/home.ts
+++ cypress/integration/home.ts
@@ -3,22 +3,6 @@
 describe('Home page', () => {
   it('should render home page sections', () => {
     cy.visit('/')
-
-    cy.get('.slick-slider').within(() => {
-      cy.findByRole('heading', { name: 'Cyberpunk 2077' })
-      cy.findByRole('link', { name: 'Buy now' })
-
-      cy.get('.slick-dots > :nth-child(2) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: 'Ghostrunner' })
-      cy.findByRole('link', { name: 'Buy now' })
-
-      cy.get('.slick-dots > :nth-child(3) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: 'Mad Max' })
-      cy.findByRole('link', { name: 'Buy now' })
-    })
+    cy.renderBanner()
   })
 })
