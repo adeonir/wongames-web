e1213c384765d3950e15a937d9f30b950992c21c
Adeonir Kohl <adeonir@gmail.com>
2022-01-31 18:15:19 -0300

    test: filter by price

--- cypress/integration/explore.ts
+++ cypress/integration/explore.ts
@@ -30,7 +30,7 @@ describe('Explore page', () => {
     cy.getByDataCy('game-card').should('have.length', 30)
   })
 
-  it('should order by price', () => {
+  it('should order by price by lowest to highest', () => {
     cy.findByText(/Lowest to highest/i).click()
     cy.location('href').should('contain', 'sort=price%3Aasc')
 
@@ -39,7 +39,9 @@ describe('Explore page', () => {
       .within(() => {
         cy.findByText('FREE').should('exist')
       })
+  })
 
+  it('should order by price by highest to lowest', () => {
     cy.findByText(/Highest to lowest/i).click()
     cy.location('href').should('contain', 'sort=price%3Adesc')
 
@@ -47,6 +49,58 @@ describe('Explore page', () => {
       .first()
       .within(() => {
         cy.shouldBeGreaterThan(0)
+      })
+  })
+
+  it('should filter by price', () => {
+    cy.findByText(/Highest to lowest/i).click()
+
+    cy.findByText(/free/i).click()
+    cy.location('href').should('contain', 'price_lte=0')
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText('FREE').should('exist')
+      })
+
+    cy.findByText('Under $50').click()
+    cy.location('href').should('contain', 'price_lte=50')
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.shouldBeLessThan(50)
+      })
+
+    cy.findByText('Under $100').click()
+    cy.location('href').should('contain', 'price_lte=100')
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.shouldBeLessThan(100)
+      })
+
+    cy.findByText('Under $150').click()
+    cy.location('href').should('contain', 'price_lte=150')
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.shouldBeLessThan(150)
+      })
+
+    cy.findByText('Under $250').click()
+    cy.location('href').should('contain', 'price_lte=250')
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.shouldBeLessThan(250)
+      })
+
+    cy.findByText('Under $500').click()
+    cy.location('href').should('contain', 'price_lte=500')
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.shouldBeLessThan(500)
       })
   })
 })
