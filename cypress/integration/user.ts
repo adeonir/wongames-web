f439c7788931c2112c9538def95ec106f702a531
Adeonir Kohl <adeonir@gmail.com>
2022-01-31 22:45:09 -0300

    test: add signup test

--- cypress/integration/user.ts
+++ cypress/integration/user.ts
@@ -1,13 +1,22 @@
 /// <reference path="../support/index.d.ts" />
 
+import { UserFactory } from '../support/factories'
+
 describe('User', () => {
   it('should sign up', () => {
+    const user = UserFactory.build()
+
     cy.visit('/sign-up')
 
-    cy.findByPlaceholderText(/Username/i).type('cypress')
-    cy.findByPlaceholderText(/email/i).type('cypress@test.com')
-    cy.findByPlaceholderText(/^password/i).type('123456')
-    cy.findByPlaceholderText(/confirm password/i).type('123456')
+    cy.findByPlaceholderText(/Username/i).type(user.username)
+    cy.findByPlaceholderText(/email/i).type(user.email)
+    cy.findByPlaceholderText(/^password/i).type(user.password)
+    cy.findByPlaceholderText(/confirm password/i).type(user.password)
     cy.findByRole('button', { name: /Sign up now/i }).click()
+
+    cy.wait(300)
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+    cy.findByText(user.username).should('exist')
   })
 })
