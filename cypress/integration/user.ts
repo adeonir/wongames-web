f7c268214b3a325c29ad0dc6d8b3d74762aaacd3
Adeonir Kohl <adeonir@gmail.com>
2022-01-31 23:13:16 -0300

    test: add signin test

--- cypress/integration/user.ts
+++ cypress/integration/user.ts
@@ -3,7 +3,7 @@
 import { UserFactory } from '../support/factories'
 
 describe('User', () => {
-  it('should sign up', () => {
+  it.skip('should sign up', () => {
     const user = UserFactory.build()
 
     cy.visit('/sign-up')
@@ -13,5 +13,23 @@ describe('User', () => {
 
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
+  })
+
+  it('should sign in/sign out', () => {
+    cy.visit('/sign-in')
+
+    cy.findByPlaceholderText(/Email/i).type('cypress@wongames.com')
+    cy.findByPlaceholderText(/^Password/i).type('123456')
+    cy.findByRole('button', { name: /Sign in now/i }).click()
+
+    cy.findByText(/Cypress/i)
+      .should('exist')
+      .click()
+    cy.findByText(/Sign out/i)
+      .should('exist')
+      .click()
+
+    cy.findByRole('link', { name: /Sign in/i }).should('exist')
+    cy.findByText(/Cypress/i).should('not.exist')
   })
 })
