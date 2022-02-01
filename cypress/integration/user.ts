0f4759188525864ea0a80f5c405959a276b4970f
Adeonir Kohl <adeonir@gmail.com>
2022-01-31 23:20:17 -0300

    test: add custom commands for sign in

--- cypress/integration/user.ts
+++ cypress/integration/user.ts
@@ -9,25 +9,19 @@ describe('User', () => {
     cy.visit('/sign-up')
     cy.signUp(user)
 
-    cy.wait(300)
-
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
     cy.findByText(user.username).should('exist')
   })
 
   it('should sign in/sign out', () => {
     cy.visit('/sign-in')
+    cy.signIn()
 
-    cy.findByPlaceholderText(/Email/i).type('cypress@wongames.com')
-    cy.findByPlaceholderText(/^Password/i).type('123456')
-    cy.findByRole('button', { name: /Sign in now/i }).click()
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
 
-    cy.findByText(/Cypress/i)
-      .should('exist')
-      .click()
-    cy.findByText(/Sign out/i)
-      .should('exist')
-      .click()
+    cy.findByText(/Cypress/i).click()
+    cy.findByText(/Sign out/i).click()
 
     cy.findByRole('link', { name: /Sign in/i }).should('exist')
     cy.findByText(/Cypress/i).should('not.exist')
