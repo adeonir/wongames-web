6a015085b8cb2436a38591098accb0eece8933b0
Adeonir Kohl <adeonir@gmail.com>
2022-02-03 10:35:35 -0300

    test: add tests to reset password with intercept and next-auth

--- cypress/integration/reset-password.ts
+++ cypress/integration/reset-password.ts
@@ -2,7 +2,7 @@
 
 describe('Reset Password', () => {
   it('should show error if password do not match', () => {
-    cy.visit('/reset-password?code=123456789')
+    cy.visit('/reset-password?code=any_code')
 
     cy.findAllByPlaceholderText(/^Password/i).type('123')
     cy.findAllByPlaceholderText(/Confirm password/i).type('321')
@@ -41,5 +41,51 @@ describe('Reset Password', () => {
     cy.findByRole('button', { name: /Reset password/i }).click()
 
     cy.findByText(/Incorrect code provided/i).should('exist')
+  })
+
+  it('should fill the input and redirect with user signed in', () => {
+    cy.intercept('POST', '**/auth/reset-password', (res) => {
+      res.reply({
+        status: 200,
+        body: {
+          user: {
+            email: 'cypress@wongames.com',
+          },
+        },
+      })
+    })
+
+    cy.intercept('POST', '**/auth/callback/credentials*', (res) => {
+      res.reply({
+        status: 200,
+        body: {
+          user: {
+            email: 'cypress@wongames.com',
+          },
+        },
+      })
+    })
+
+    cy.intercept('GET', '**/auth/session*', (res) => {
+      res.reply({
+        status: 200,
+        body: {
+          user: {
+            name: 'cypress',
+            email: 'cypress@wongames.com',
+          },
+        },
+      })
+    })
+
+    cy.visit('/reset-password?code=valid_code')
+
+    cy.findAllByPlaceholderText(/^Password/i).type('123')
+    cy.findAllByPlaceholderText(/Confirm password/i).type('123')
+
+    cy.findByRole('button', { name: /Reset password/i }).click()
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+    cy.findByText(/Cypress/i).should('exist')
   })
 })
